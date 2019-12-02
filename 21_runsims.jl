5a004fead68e9d0f26e216b99992e4fe17af8f6d
John Best <jkbest@gmail.com>
2019-12-02 11:57:40 -0800

    Don't plot while generating simulated data sets

--- 21_runsims.jl
+++ 21_runsims.jl
@@ -1,9 +1,4 @@
 pmap(1:100) do repl
     Pdict, Cdict = run_scenarios(repl, "prep.h5")
     save_scenarios(repl, Pdict, Cdict)
-    for scen in keys(Pdict)
-        gif_file = "repl_" * string(repl, pad = 2) * "/" *
-                    string(scen) * ".gif"
-        plot_gif(Pdict[scen], Cdict[scen], gif_file)
-    end
 end
