cd9c08c350caad19c0b9270a55c06a59e721355c
John Best <jkbest@gmail.com>
2021-02-17 17:07:36 -0800

    Generate simulations sequentially for now

--- 22_runsims_sharedq.jl
+++ 22_runsims_sharedq.jl
@@ -2,6 +2,10 @@ using Distributed
 
 n_sims = parse(Int, ARGS[1])
 
-pmap(1:n_sims) do repl
+# pmap(1:n_sims) do repl
+#     run_sharedq_sim(repl, "prep.h5")
+# end
+
+for repl in 1:n_sims
     run_sharedq_sim(repl, "prep.h5")
 end
