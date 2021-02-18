efe06ae42a9e1bd048bfc09d8a93db1ecff1ce10
John Best <jkbest@gmail.com>
2021-02-18 15:55:07 -0800

    Add pref intensity sim scripts

---
+++ 23_runsims_prefintensity.jl
@@ -0,0 +1,11 @@
+using Distributed
+
+n_sims = parse(Int, ARGS[1])
+
+# pmap(1:n_sims) do repl
+#     run_sharedq_sim(repl, "prep.h5")
+# end
+
+for repl in 1:n_sims
+    run_prefintensity_sim(repl, "prep.h5")
+end
