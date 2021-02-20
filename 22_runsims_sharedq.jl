fc580c54446cedb84fe1eeed62251687def309a5
John Best <jkbest@gmail.com>
2021-02-19 20:51:50 -0800

    Update to run_sims

--- 22_runsims_sharedq.jl
+++ 22_runsims_sharedq.jl
@@ -2,10 +2,12 @@ using Distributed
 
 n_sims = parse(Int, ARGS[1])
 
+run_sims(SharedQSpec, n_sims)
+
 # pmap(1:n_sims) do repl
 #     run_sharedq_sim(repl, "prep.h5")
 # end
 
-for repl in 1:n_sims
-    run_sharedq_sim(repl, "prep.h5")
-end
+# for repl in 1:n_sims
+#     run_sims(SharedQSpec, repl, "prep.h5")
+# end
--- 23_runsims_prefintensity.jl
+++ 23_runsims_prefintensity.jl
@@ -2,10 +2,12 @@ using Distributed
 
 n_sims = parse(Int, ARGS[1])
 
+run_sims(PrefIntensitySpec, n_sims)
+
 # pmap(1:n_sims) do repl
 #     run_sharedq_sim(repl, "prep.h5")
 # end
 
-for repl in 1:n_sims
-    run_prefintensity_sim(repl, "prep.h5")
-end
+# for repl in 1:n_sims
+#     run_prefintensity_sim(repl, "prep.h5")
+# end
