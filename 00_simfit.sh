ce4434c1eb507a2ac10fdc67368151cbf8f6e24e
John Best <jkbest@gmail.com>
2021-03-26 10:00:04 -0700

    Use --parsable to get SLURM job ids

--- 00_simfit.sh
+++ 00_simfit.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-qdevscalingsim_jid=$(sbatch 21_runsims_qdevscaling.slurm)
-qdevscalingfit_jid=$(sbatch --dependency=afterok:$qdevscalingsim_jid 31_fit_qdevscaling_parallel_ckpt.slurm)
+qdevscalingsim_jid=$(sbatch --parsable 21_runsims_qdevscaling.slurm)
+qdevscalingfit_jid=$(sbatch --parsable --dependency=afterok:$qdevscalingsim_jid 31_fit_qdevscaling_parallel_ckpt.slurm)
 
-sharedqsim_jid=$(sbatch 21_runsims_sharedq.slurm)
-sharedqfit_jid=$(sbatch --dependency=afterok:$sharedqsim_jid 31_fit_sharedq_parallel_ckpt.slurm)
+sharedqsim_jid=$(sbatch --parsable 22_runsims_sharedq.slurm)
+sharedqfit_jid=$(sbatch --parsable --dependency=afterok:$sharedqsim_jid 32_fit_sharedq_parallel_ckpt.slurm)
 
-prefintensitysim_jid=$(sbatch 21_runsims_prefintensity.slurm)
-prefintensityfit_jid=$(sbatch --dependency=afterok:$prefintensitysim_jid 31_fit_prefintensity_parallel_ckpt.slurm)
+prefintensitysim_jid=$(sbatch --parsable 23_runsims_prefintensity.slurm)
+prefintensityfit_jid=$(sbatch --parsable --dependency=afterok:$prefintensitysim_jid 33_fit_prefintensity_parallel_ckpt.slurm)
 
 sbatch --dependency=afterok:$qdevscalingfit_jid:$sharedqfit_jid:$prefintensityfit_jid 40_readfits.slurm
 
