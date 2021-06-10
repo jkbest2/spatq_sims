27bb8b03808a77aed6a217cb34912a706883cd98
John Best <jkbest@gmail.com>
2021-06-10 09:56:22 -0700

    Add density-dependent catchability simstudy

--- 00_simfit.sh
+++ 00_simfit.sh
@@ -9,7 +9,10 @@ sharedqfit_jid=$(sbatch --parsable --dependency=afterok:$sharedqsim_jid 32_fit_s
 prefintensitysim_jid=$(sbatch --parsable 23_runsims_prefintensity.slurm)
 prefintensityfit_jid=$(sbatch --parsable --dependency=afterok:$prefintensitysim_jid 33_fit_prefintensity_parallel_ckpt.slurm)
 
-sbatch --dependency=afterok:$qdevscalingfit_jid:$sharedqfit_jid:$prefintensityfit_jid 40_readfits.slurm
+densdepqsim_jid=$(sbatch --parsable 24_runsims_densdepq.slurm)
+densdepqfit_jid=$(sbatch --parsable --dependency=afterok:$densdepqsim_jid 34_fit_densdepq_parallel_ckpt.slurm)
+
+sbatch --dependency=afterok:$qdevscalingfit_jid:$sharedqfit_jid:$prefintensityfit_jid:$densdepqfit_jid 40_readfits.slurm
 
 # show dependencies in squeue output:
 squeue -u $USER -o "%.8A %.4C %.10m %.20E"
---
+++ 24_runsims_densdepq.jl
@@ -0,0 +1,7 @@
+using Distributed
+
+n_sims = parse(Int, ARGS[1])
+
+pmap(1:n_sims) do rlz
+    run_sims(DensityDependentQSpec, rlz)
+end
