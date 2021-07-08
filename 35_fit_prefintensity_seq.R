9a735b879755aa9bcca86ce0715676ab10ebebce
John Best <jkbest@gmail.com>
2021-07-08 14:11:17 -0700

    Add survey models to simple prefintensity series

--- 35_fit_prefintensity_seq.R
+++ 35_fit_prefintensity_seq.R
@@ -25,18 +25,28 @@ optcontrol <- list(eval.max = 1000L, iter.max = 750L)
 ## Names of the operating models
 opmods <- 1:6
 ## Names of the estimation models
-estmods <- "model"
-estd <- specify_estimated(beta = TRUE,
-                          lambda = FALSE,
-                          obs_lik = 1L)
+estmods <- c("design", "model", "survey")
+estd_mod <- function(em) {
+  switch(em,
+         design = specify_estimated(beta = FALSE,
+                                    obs_lik = 1L),
+         model = specify_estimated(beta = TRUE,
+                                   lambda = FALSE,
+                                   obs_lik = 1L),
+         survey = specify_estimated(beta = TRUE,
+                                    omega = TRUE,
+                                    lambda = FALSE,
+                                    obs_lik = 1L))
+}
+
 
 spec_df <- cross_df(list(study = study,
                          repl = repls,
                          opmod = opmods,
                          estmod = estmods))
 
-design_estimator <- function(setup, max_T = 15) {
-  mat <- matrix(setup$data$catch_obs, ncol = max_T)
+design_estimator <- function(catch_df, max_T = 15) {
+  mat <- matrix(catch_df$catch_biomass, ncol = max_T)
   index <- colMeans(mat)
   names(index) <- rep_along(index, "Index")
   sd <- apply(mat, 2, sd) / sqrt(nrow(mat))
@@ -49,30 +59,40 @@ design_estimator <- function(setup, max_T = 15) {
 
 for (i in 1:nrow(spec_df)) {
   spec <- spatq_simstudyspec(as.list(spec_df[i, ]))
-  setup <- spatq_simsetup(study = spec$study,
-                          repl = spec$repl,
-                          opmod = spec$opmod,
-                          sub_df = data.frame(vessel_idx = 2, n = 0),
-                          max_T = max_T,
-                          spec_estd = estd)
+  if (spec$estmod == "design") {
+    catch_df <- read_catch(spec$study,
+                           repl = spec$repl,
+                           opmod = spec$opmod,
+                           root_dir = root_dir,
+                           feather = TRUE) %>%
+      filter(vessel == "Survey",
+             year <= max_T)
 
-  ## Calculate and save design estimator
-  dest <- design_estimator(setup, max_T)
-  spec0 <- spec
-  spec0$estmod <- "design"
-  save_index(spec0, dest, max_T)
+    ## Calculate and save design estimator
+    dest <- design_estimator(catch_df, max_T)
+    spec0 <- spec
+    spec0$estmod <- "design"
+    save_index(spec0, dest, max_T)
+  } else {
+    estd <- estd_mod(spec$estmod)
+    setup <- spatq_simsetup(study = spec$study,
+                            repl = spec$repl,
+                            opmod = spec$opmod,
+                            sub_df = data.frame(vessel_idx = 2, n = 0),
+                            max_T = max_T,
+                            spec_estd = estd)
+    ## Now simplest model-based estimator
+    obj <- spatq_obj(setup)
+    fit <- spatq_fit(obj)
+    fit <- spatq_fit(obj, fit = fit, method = "BFGS")
+    fit <- spatq_fit(obj, fit = fit)
+    sdr <- sdreport_spatq(obj)
+    sdr$unbiased <- list(value = sdr$value,
+                         sd = sdr$sd)
+    rep <- report_spatq(obj)
+    lpb <- obj$env$last.par.best
 
-  ## Now simplest model-based estimator
-  obj <- spatq_obj(setup)
-  fit <- spatq_fit(obj)
-  fit <- spatq_fit(obj, fit = fit, method = "BFGS")
-  fit <- spatq_fit(obj, fit = fit)
-  sdr <- sdreport_spatq(obj)
-  sdr$unbiased <- list(value = sdr$value,
-                       sd = sdr$sd)
-  rep <- report_spatq(obj)
-  lpb <- obj$env$last.par.best
-
-  save_fit(spec, fit, lpb, rep, sdr)
-  save_index(spec, sdr, max_T)
+    save_fit(spec, fit, lpb, rep, sdr)
+    save_index(spec, sdr, max_T)
+  }
 }
