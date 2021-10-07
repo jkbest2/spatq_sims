d20ec5867a33551b504cca110f7ccee69747d74d
John Best <jkbest@gmail.com>
2021-10-07 14:34:07 -0700

    Add prep dep to 00_runsims script

--- 00_simfit.sh
+++ 00_simfit.sh
@@ -1,27 +1,29 @@
 #! /bin/bash
 
-qdevscalingsim_jid=$(sbatch --parsable 21_runsims_qdevscaling.slurm)
+prepsims_jid=$(sbatch --parsable 10_prepsims.slurm)
+
+qdevscalingsim_jid=$(sbatch --parsable --dependency=afterok:$prepsims_jid 21_runsims_qdevscaling.slurm)
 qdevscalingfit_jid=$(sbatch --parsable --dependency=afterok:$qdevscalingsim_jid 31_fit_qdevscaling_ckpt.slurm)
 sbatch --dependency=singleton 31_fit_qdevscaling_ckpt.slurm
 sbatch --dependency=singleton 31_fit_qdevscaling_ckpt.slurm
 sbatch --dependency=singleton 31_fit_qdevscaling_ckpt.slurm
 sbatch --dependency=singleton 31_fit_qdevscaling_ckpt.slurm
 
-# sharedqsim_jid=$(sbatch --parsable 22_runsims_sharedq.slurm)
+# sharedqsim_jid=$(sbatch --parsable --dependency=afterok:$prepsims_jid 22_runsims_sharedq.slurm)
 # sharedqfit_jid=$(sbatch --parsable --dependency=afterok:$sharedqsim_jid 32_fit_sharedq_ckpt.slurm)
 # sbatch --dependency=singleton 32_fit_sharedq_ckpt.slurm
 # sbatch --dependency=singleton 32_fit_sharedq_ckpt.slurm
 # sbatch --dependency=singleton 32_fit_sharedq_ckpt.slurm
 # sbatch --dependency=singleton 32_fit_sharedq_ckpt.slurm
 
-prefintensitysim_jid=$(sbatch --parsable 23_runsims_prefintensity.slurm)
+prefintensitysim_jid=$(sbatch --parsable --dependency=afterok:$prepsims_jid 23_runsims_prefintensity.slurm)
 prefintensityfit_jid=$(sbatch --parsable --dependency=afterok:$prefintensitysim_jid 33_fit_prefintensity_ckpt.slurm)
 sbatch --dependency=singleton 33_fit_prefintensity_ckpt.slurm
 sbatch --dependency=singleton 33_fit_prefintensity_ckpt.slurm
 sbatch --dependency=singleton 33_fit_prefintensity_ckpt.slurm
 sbatch --dependency=singleton 33_fit_prefintensity_ckpt.slurm
 
-densdepqsim_jid=$(sbatch --parsable 24_runsims_densdepq.slurm)
+densdepqsim_jid=$(sbatch --parsable --dependency=afterok:$prepsims_jid 24_runsims_densdepq.slurm)
 densdepqfit_jid=$(sbatch --parsable --dependency=afterok:$densdepqsim_jid 34_fit_densdepq_ckpt.slurm)
 sbatch --dependency=singleton 34_fit_densdepq_ckpt.slurm
 sbatch --dependency=singleton 34_fit_densdepq_ckpt.slurm
