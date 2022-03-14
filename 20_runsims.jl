5fb59f358e3ee0089e6b8c6c6b417f310b9c36c2
John Best <jkbest@gmail.com>
2022-03-14 15:22:10 -0700

    Use pmap for simulations

--- 20_runsims.jl
+++ 20_runsims.jl
@@ -1,4 +1,5 @@
-using SpatQSims
+@everywhere using Distributed
+@everywhere using SpatQSims
 
 simtypes = [
     QDevScalingSpec,
@@ -11,9 +12,16 @@ simtypes = [
 
 repls = 1:100
 
-for simtype in simtypes
-    run_sims(simtype,
-             repls;
+specs  = Any[]
+for st in simtypes
+    svals = sim_values(st)
+    for r in repls, sv in svals
+        push!(specs, st(r, sv))
+    end
+end
+
+pmap(specs) do sp
+    run_sims([sp];
              checkpoint = true,
              csv = false,
              feather = true)
