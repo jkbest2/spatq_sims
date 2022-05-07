dc95372f3e53f0137b0eb72646afaa6106190818
John Best <jkbest@gmail.com>
2022-05-06 20:31:22 -0700

    Add bycatch scenario to om_results

--- 47_om_results.R
+++ 47_om_results.R
@@ -3,13 +3,17 @@ library(tidyverse)
 library(patchwork)
 library(hdf5r)
 
+source("40_postproc_funs.R")
+
 root_dir <- "."
 eval_dir <- "om_results"
+if (!dir.exists(eval_dir)) dir.create(eval_dir)
 ## Which simulation study are we fitting?
 studies <- c("qdevscaling",
              "habq",
              "prefintensity",
-             "densdepq")
+             "densdepq",
+             "bycatch")
 names(studies) <- studies # So `lapply` outputs named list
 ## What range of replicates are going to be fit?
 repls <- factor(1:100)
@@ -17,34 +21,6 @@ repls <- factor(1:100)
 max_T <- 15
 ## Names of the operating models
 opmods <- factor(1:6)
-
-get_om_parval <- function(study, opmod = 1:6) {
-  qds <- 10 ^ (seq(-3, -0.5, 0.5))
-  qdcv <- sqrt(exp(qds ^ 2) - 1)
-  studyvals <- switch(study,
-                      qdevscaling = qdcv,
-                      habq = 1.75 ^ (-2:3),
-                      prefintensity = c(0, 1, 2, 4, 8, 16),
-                      densdepq = seq(0, 1.25, 0.25))
-  studyvals[opmod]
-}
-
-get_om_parlabel <- function(study) {
-  switch(study,
-         qdevscaling = "Spatial catchability CV",
-         habq = "Relative habitat preference",
-         prefintensity = "Preference power",
-         densdepq = "Density dependent multiplier")
-}
-
-get_study_title <- function(study) {
-  switch(study,
-         qdevscaling = "a. Spatial catchability variability",
-         habq = "b. Habitat-dependent catchability",
-         prefintensity = "c. Preference intensity",
-         densdepq = "d. Density-dependent catchability")
-}
-
 
 spec_df <- cross_df(list(study = studies,
                          repl = repls,
@@ -62,16 +38,6 @@ pop_df <- spec_df %>%
   select(-has_pop_feather, -has_pop_h5) %>%
   unnest(pop) %>%
   filter(year <= 15)
-
-## pop_traj <- ggplot(pop_df, aes(x = year, y = pop, group = repl)) +
-##   geom_line(alpha = 0.2) +
-##   facet_grid(studyf ~ opmod) +
-##   ylim(0, 100) +
-##   scale_x_continuous(name = "Year",  c(5, 10, 15)) +
-##   theme_bw()
-## ggsave(file.path(eval_dir, "pop_trajectories.svg"),
-##        pop_traj,
-##        width = 8, height = 6)
 
 traj_plot <- function(data) {
   data %>%
